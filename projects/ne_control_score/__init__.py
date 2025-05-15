6df5bb4d08060c534d7fca002cd4936889ea713c
Maximilien Le Clei <github.laborer138@passmail.net>
2025-05-15 20:28:36 +0000

    test

--- projects/ne_control_score/__init__.py
+++ projects/ne_control_score/__init__.py
@@ -1,12 +1,7 @@
 """Score Optimization w/ Neuroevolution on Gym control environments."""
 
-from beartype import BeartypeConf
-from beartype.claw import beartype_this_package
-
 from .agent import GymAgent, GymAgentConfig
 from .space import GymReinforcementSpace, GymReinforcementSpaceConfig
-
-beartype_this_package(conf=BeartypeConf(is_pep484_tower=True))
 
 __all__ = [
     "GymAgent",
